536aadd0491692e61bc29d129f249f1b99757d85
Akarsh <akarshsingh81@gmail.com>
2023-07-02 15:16:41 +0530

    start project

---
+++ src/main/java/com/example/ShopperSMart/ShopperSMartApplication.java
@@ -0,0 +1,14 @@
+package com.example.ShopperSMart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopperSMartApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ShopperSMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ShopperSMart/ShopperSMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ShopperSMart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopperSMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
